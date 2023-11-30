657abaffb43145d409b64164a1c00ae33b6e70f7
Adrian Alfred C. Bonpin <adrianbonpin@gmail.com>
2023-11-30 22:59:18 +0800

    update

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+import styles from './page.module.css'
+import Nav from '@/app/components/navbar'
+import ThemeSwitcher from '@/app/components/themeSwitcher'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Nav/>
+      <ThemeSwitcher/>
+      <div className={styles.temp}>
+        About Page
+      </div>
+    </main>
+  )
+}
