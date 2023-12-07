35200c0aba2186861fc24b550927fd727b4d451d
Adrian Alfred C. Bonpin <adrianbonpin@gmail.com>
2023-12-07 21:30:58 +0800

    distance, duration, and cost metrics updated

--- src/app/components/map.js
+++ src/app/components/map.js
@@ -1,12 +1,16 @@
 'use client'
 
-import {  useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer, } from "@react-google-maps/api";
-import { useRef, useState }  from "react";
+import {  useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer, useGoogleMap } from "@react-google-maps/api";
+import { useEffect, useRef, useState }  from "react";
 import styles from "@/app/components/map.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationCrosshairs } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyBF8tm3ds5kJQ7l7VbQcqWQQZkUy1AFgKM",
+    libraries: ["places"],
+  });
 
   const [ center, setCenter ] = useState({ lat: 10.332615673533242, lng: 123.90974285444454 })
 
@@ -14,7 +18,17 @@ export default function App() {
 
   // For getting location
 
-  const [ location, setLocation ] = useState()
+  const [ location, setLocation ] = useState(0)
+
+  const [ metrics, setMetrics ] = useState(0)
+
+  const geocoder = useRef(null)
+
+  useEffect(() => {
+    if (isLoaded) {
+      geocoder.current = new window.google.maps.Geocoder()
+    }
+  }, [isLoaded])
 
   const getLoc = async () => {
     try {
@@ -27,17 +41,18 @@ export default function App() {
       })
       setCenter({ lat: position.coords.latitude, lng: position.coords.longitude })
       setZoom(16)
-      console.log(latitude)
-
+      setLocation(1)
+      if (geocoder.current) {
+        geocoder.current.geocode({ location: center}, (res, stat) => {
+          if (stat === 'OK') {
+            console.log(res[0].formatted_address)
+          }
+        })
+      }
     } catch (error) {
       console.error('Error getting location')
     }
   }
-  
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: "AIzaSyBF8tm3ds5kJQ7l7VbQcqWQQZkUy1AFgKM",
-    libraries: ["places"],
-  });
 
   const [map, setMap] = useState(/** @type google.maps.Map */ (null));
   const [directionsResponse, setDirectionsResponse] = useState(null);
@@ -66,6 +81,8 @@ export default function App() {
       travelMode: google.maps.TravelMode.TRANSIT,
     });
     setDirectionsResponse(results);
+    setLocation(0)
+    setMetrics(1)
     setDistance(results.routes[0].legs[0].distance.text);
     setDuration(results.routes[0].legs[0].duration.text);
   }
@@ -74,9 +91,18 @@ export default function App() {
     setDirectionsResponse(null);
     setDistance("");
     setDuration("");
+    setLocation(0)
+    setMetrics(0)
     originRef.current.value = "";
     destiantionRef.current.value = "";
   }
+
+  const cebuBounds = {
+    north: 10.3597,
+    south: 10.2257,
+    east: 123.9227,
+    west: 123.8001,
+  };
 
   return (
     <div className={styles.container}>
@@ -85,7 +111,7 @@ export default function App() {
       </button>
       <div className={styles.overlay}>
         <div className={styles.inputContainer}>
-          <Autocomplete>
+          <Autocomplete bounds={cebuBounds}>
             <input
               type="text"
               placeholder="Start Point"
@@ -93,7 +119,7 @@ export default function App() {
               className={styles.textInput}
             />
           </Autocomplete>
-          <Autocomplete>
+          <Autocomplete bounds={cebuBounds}>
             <input
               type="text"
               placeholder="End Point"
@@ -116,41 +142,70 @@ export default function App() {
             Clear
           </button>
         </div>
-        <div className={styles.distanceDurationContainer}>
-          <div className={styles.col_2}>
-            <span>Distance:</span><h3 className={styles.calc}>{distance}</h3>
+        <div className={metrics === 0 ? styles.hide : styles.metrics }>
+          {/* Distance & Duration */}
+          <div className={styles.tblHead}>
+            <div className={styles.col_2}>
+              <span className={styles.tblHeadtxt}>Distance</span>
+            </div>
+            <div className={styles.col_2}>
+              <span className={styles.tblHeadtxt}>Duration</span>
+            </div>
           </div>
-          <div className={styles.col_2}>
-            <span>Duration:</span><h3 className={styles.calc}>{duration}</h3>
+          <div className={styles.tblCont}>
+            <div className={styles.col_2}>
+              <span className={styles.tbltxt}>{distance}</span>
+            </div>
+            <div className={styles.col_2}>
+              <span className={styles.tbltxt}>{duration}</span>
+            </div>
           </div>
-        </div>
-        {/* Add Cost */}
-        <div className={styles.costCont}>
-          <div className={styles.col_2}>
-            <span>Traditional</span>
+          {/* Cost */}
+          <div className={styles.tblHead}>
+            <div className={styles.col_3}>
+
+            </div>
+            <div className={styles.col_3}>
+              <span className={styles.tblHeadtxt}>Traditional</span>
+            </div>
+            <div className={styles.col_3}>
+              <span className={styles.tblHeadtxt}>Modern</span>
+            </div>
           </div>
-          <div className={styles.col_2}>
-            <span>Modern</span>
+          <div className={styles.tblCont}>
+            <div className={styles.col_3}>
+              <span className={styles.tbltxt}>Standard</span>
+            </div>
+            <div className={styles.col_3}>
+              <span className={styles.tbltxt}>Php {15 + (distance < 4 ? 0 : (distance - 4) * 1.5)}</span>
+            </div>
+            <div className={styles.col_3}>
+              <span className={styles.tbltxt}>Php {13 + (distance < 4 ? 0 : (distance - 4) * 1.5)}</span>
+            </div>
+          </div>
+          <div className={styles.tblCont}>
+            <div className={styles.col_3}>
+              <span className={styles.tbltxt}>Discount</span>
+            </div>
+            <div className={styles.col_3}>
+              <span className={styles.tbltxt}>Php {12 + (distance < 4 ? 0 : (distance - 4) * 1.5)}</span>
+            </div>
+            <div className={styles.col_3}>
+              <span className={styles.tbltxt}>Php {11 + (distance < 4 ? 0 : (distance - 4) * 1.5)}</span>
+            </div>
           </div>
         </div>
       </div>
       <div className={styles.mapContainer}>
         {/* Google Map Box */}
-        <GoogleMap
-          center={center}
-          zoom={zoom}
-          mapContainerStyle={{ width: "100%", height: "100%" }}
-          options={{
+        <GoogleMap center={center} zoom={zoom} mapContainerStyle={{ width: "100%", height: "100%" }} options={{
             zoomControl: false,
-            streetViewControl: false,
+            streetViewControl:false,
             mapTypeControl: false,
-            fullscreenControl: false,
-          }}
-          onLoad={(map) => setMap(map)}
-        >
-          {directionsResponse && (
-            <DirectionsRenderer directions={directionsResponse} />
-          )}
+            fullscreenControl: false, }}
+          onLoad={(map) => setMap(map)}>
+            <Marker position={location === 0 ? null : center} />
+            {directionsResponse && ( <DirectionsRenderer directions={directionsResponse} /> )}
         </GoogleMap>
       </div>
     </div>
